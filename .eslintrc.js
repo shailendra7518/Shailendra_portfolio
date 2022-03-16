2e0397c543f7c268aabc540ba02783d07d711f66
Your Name <shailendraftp0001@gmail.com>
2022-03-16 11:51:23 +0530

    adde

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    // we only want single quotes
+  // env: {
+  //   browser: true,
+  //   es2021: true,
+  // },
+  // extends: [
+  //   'plugin:react/recommended',
+  //   'airbnb',
+  // ],
+  // parserOptions: {
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  //   ecmaVersion: 12,
+  //   sourceType: 'module',
+  // },
+  // plugins: [
+  //   'react',
+  // ],
+  // rules: {
+  //   // we only want single quotes
     
-    quotes: "off",
-    // we want to force semicolons
-    semi: 'off',
-    // we use 2 spaces to indent our code
-    indent: "off",
-    // we want to avoid useless spaces
-    'no-multi-spaces': 'off',
-    'jsx-quotes': 'off',
-    'react/prop-types': 'off',
-    'import/prefer-default-export': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-filename-extension': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    "indent": "off",
-    "react/jsx-indent": "off",
-    "react/jsx-indent-props": "off"
-  },
+  //   quotes: "off",
+  //   // we want to force semicolons
+  //   semi: 'off',
+  //   // we use 2 spaces to indent our code
+  //   indent: "off",
+  //   // we want to avoid useless spaces
+  //   'no-multi-spaces': 'off',
+  //   'jsx-quotes': 'off',
+  //   'react/prop-types': 'off',
+  //   'import/prefer-default-export': 'off',
+  //   'react/no-unescaped-entities': 'off',
+  //   'react/react-in-jsx-scope': 'off',
+  //   'react/jsx-props-no-spreading': 'off',
+  //   'react/jsx-filename-extension': 'off',
+  //   'import/no-extraneous-dependencies': 'off',
+  //   "indent": "off",
+  //   "react/jsx-indent": "off",
+  //   "react/jsx-indent-props": "off"
+  // },
 };
